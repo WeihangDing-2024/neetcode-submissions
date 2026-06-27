68ee72e953794b7f2d08d0861ef06f4c0b8ff366
Weihang Ding <may971119@gmail.com>
2026-06-27 11:12:10 +0800

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,38 @@
+class PrefixTree:
+
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+        
+
+    def insert(self, word: str) -> None:
+        curr = self
+    
+        for char in word:
+            children = curr.children
+            if not char in children:
+                children[char] = Trie()
+            curr = children[char]
+
+        curr.endOfWord = True
+        
+
+    def search(self, word: str) -> bool:
+        curr = self
+        for char in word:
+            children = curr.children
+            if not char in children:
+                return False
+            curr = children[char]
+        return curr.endOfWord
+
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self
+        for char in prefix:
+            children = curr.children
+            if not char in children:
+                return False
+            curr = children[char]
+        return True
+        
